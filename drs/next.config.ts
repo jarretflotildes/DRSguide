f0c1ae70d823d5414698e62fe6fe242b1d78b764
Mona <jflotildes@utexas.edu>
2025-08-04 02:34:17 -0500

    test

--- drs/next.config.ts
+++ drs/next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
-// If you have this in next.config.js:
 module.exports = {
-  distDir: 'build', // This would create 'build' folder instead of '.next'
+  distDir: 'build'
 }
 
 const nextConfig: NextConfig = {  
