6e19729c9f39970da902d51c5d824765f31218be
Mona <jflotildes@utexas.edu>
2025-08-04 02:24:16 -0500

    netifly

--- drs/next.config.ts
+++ drs/next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
+// If you have this in next.config.js:
+module.exports = {
+  distDir: 'build', // This would create 'build' folder instead of '.next'
+}
+
 const nextConfig: NextConfig = {  
 };
 
